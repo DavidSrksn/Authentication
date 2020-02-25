dd542b56059f845957d579dc5f8a18bac30c26a0
DavidSrksn <s.david99@mail.ru>
2020-02-25 22:01:23 +0300

    Initial Commit

---
+++ Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Authentication
+//
+//  Created by David Sarkisyan on 25.02.2020.
+//  Copyright © 2020 DavidS & that's all. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
